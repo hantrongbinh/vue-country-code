72404145ed2db2fbead7a5b107907ad40c040e02
Trong Binh Han <me@hantrongbinh.dev>
2020-06-29 10:24:13 +0700

    update gh-pages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: ["plugin:vue/essential", "@vue/prettier"],
+  // plugins: ["prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
   parserOptions: {
-    parser: "babel-eslint",
-  },
+    parser: "babel-eslint"
+  }
 };
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  outputDir: "docs"
+  outputDir: "docs",
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-country-code/" : "/"
 };
